24aa44cce093e8ac5d869c19536a4669ceb8110e
Ninism <nin.alamo@outlook.com>
2026-09-24 22:18:52 +0800

    Update async-storage.ts

--- __mocks__/@react-native-async-storage/async-storage.ts
+++ __mocks__/@react-native-async-storage/async-storage.ts
@@ -25,12 +25,12 @@ const AsyncStorage = {
     store.clear();
   }),
 
-  getItem: jest.fn(async (key: string) => store.get(key) || null),
-  setItem: jest.fn(async (key: string, value: string) => { store.set(key, value); }),
-  removeItem: jest.fn(async (key: string) => { store.delete(key); }),
-  multiRemove: jest.fn(async (keys: string[]) => keys.forEach((k: string) => store.delete(k))),
-  getAllKeys: jest.fn(async () => Array.from(store.keys())),
-  clear: jest.fn(async () => store.clear()),
+  //getItem: jest.fn(async (key: string) => store.get(key) || null),
+  //setItem: jest.fn(async (key: string, value: string) => { store.set(key, value); }),
+  //removeItem: jest.fn(async (key: string) => { store.delete(key); }),
+  //multiRemove: jest.fn(async (keys: string[]) => keys.forEach((k: string) => store.delete(k))),
+  //getAllKeys: jest.fn(async () => Array.from(store.keys())),
+  //clear: jest.fn(async () => store.clear()),
   __store: store,
 };
 
